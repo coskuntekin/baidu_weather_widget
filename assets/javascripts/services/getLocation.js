b99077d323d1b617fe6e32338788e33139cf8e6e
coskuntekin <mail@coskuntekin.com>
2017-06-29 10:34:18 +0800

    Automated commit at 2017-06-29 02:34:18 UTC by middleman-deploy 2.0.0-alpha

--- assets/javascripts/services/getLocation.js
+++ assets/javascripts/services/getLocation.js
@@ -1,1 +1,1 @@
-angular.module("baiduWeatherWidgetApp").factory("locationService",["$http",function(t){return{getLocation:function(){return t({method:"JSONP",url:"http://api.map.baidu.com/location/ip?ak=hBDoMmfaQvkxwifiKdsQij6s"}).then(function(t){return t.data},function(t){return t.data})}}}]);
+"use strict";angular.module("baiduWeatherWidgetApp").factory("locationService",["$http",function(t){return{getLocation:function(){return t({method:"JSONP",url:"http://api.map.baidu.com/location/ip?ak=hBDoMmfaQvkxwifiKdsQij6s"}).then(function(t){return t.data},function(t){return t.data})}}}]);
